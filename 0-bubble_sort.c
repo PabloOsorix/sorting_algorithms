8df5900b0bd442729e19dc2493364993638997b8
PabloOsorix <3387@holbertonschool.com>
2022-02-11 00:21:57 -0500

    :heavy_plus_sign: Write function that use Bubble_sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ *bubble_sort - Function  that sorts an array of integers
+ *in ascending order using the Bubble sort algorithm.
+ *@array: array to sort with the algorithm.
+ *@size: size of the array
+ *Return - None, function is void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    int i;
+    int tmp = 0;
+    int counter = 1;
+
+    if (array == NULL || size == 0)
+        exit(98);
+
+    while(counter != 0)
+    {   
+        counter = 0;
+        for (i = 0; array && i < size; ++i)
+        {
+            
+            if (array[i] < array[i - 1])
+            {
+                counter+= 1;
+                tmp = array[i];
+                array[i] = array[i - 1];
+                array[i - 1] = tmp;
+                
+                print_array(array, size);
+            }
+            else
+                continue;
+        }
+    }
+}
