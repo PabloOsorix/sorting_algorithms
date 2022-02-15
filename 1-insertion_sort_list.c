f5fb0fc69f680a7e75994ec0a0e0bf92cb301161
PabloOsorix <3387@holbertonschool.com>
2022-02-15 18:47:23 -0500

    :pencil2: fix betty requeriments.

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,90 +10,90 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *tmp = NULL, *s_tmp = NULL,*head = NULL;
-    int counter = 1;
-    head = malloc(sizeof(listint_t));
-    tmp = malloc(sizeof(listint_t));
-    s_tmp = malloc(sizeof(listint_t));
-    if (!tmp || !head)
-        return;
+	listint_t *tmp = NULL, *s_tmp = NULL,*head = NULL;
+	int counter = 1;
+	head = malloc(sizeof(listint_t));
+	tmp = malloc(sizeof(listint_t));
+	s_tmp = malloc(sizeof(listint_t));
+	if (!tmp || !head || !s_tmp)
+		return;
 
-    if ((!list || !(*list)->n))
-        return;
+	if ((!list || !(*list)->n))
+		return;
 
-    head = *list;
-    while(counter != 0)
-    {
-        counter = 0;
-        for (;(*list)->next != NULL;)
-        {
-            if ((*list)->next != NULL && (*list)->prev == NULL)
-            {
-                if((*list)->n > (*list)->next->n)
-                {
-                    tmp = (*list)->next;
-                    (*list)->next->next->prev = *list;
-                    (*list)->next = (*list)->next->next;
-                    (*list)->prev = tmp;
-                    tmp->next = *list;
-                    tmp->prev  = NULL;
-                    *list = tmp;
-                    head = *list;
-                    print_list(head);
-                    counter+= 1;
-                    *list = (*list)->next;
-                    continue;
-                }
-            }
-            if((*list)->n > (*list)->next->n)
-            {
-                s_tmp = (*list)->next;
-                s_tmp->prev = (*list)->prev;
-                (*list)->next = (*list)->next->next;
-                (*list)->prev->next = s_tmp;
-                (*list)->prev = s_tmp;
-                s_tmp->next = *list;
-                *list = s_tmp;
-                print_list(head);
-                counter+= 1;
-                if ((*list)->prev->n > (*list)->n)
-                {
-                    while((*list)->n < (*list)->prev->n)
-                    {
-                        tmp = (*list)->prev;
-                        if (tmp->prev != NULL)
-                        {
-                            tmp->prev->next = *list;
-                            (*list)->prev = (*list)->prev->prev;
-                            if ((*list)->next != NULL)
-                            {
-                                tmp->next = (*list)->next;
-                                (*list)->next->prev = tmp;
-                            }
-                            else
-                                tmp->next = NULL;
-                            (*list)->next = tmp;
-                            tmp->prev = *list;
-                            print_list(head);
+	head = *list;
+	while(counter != 0)
+	{
+		counter = 0;
+		for (;(*list)->next != NULL;)
+		{
+			if ((*list)->next != NULL && (*list)->prev == NULL)
+			{
+				if((*list)->n > (*list)->next->n)
+				{
+					tmp = (*list)->next;
+					(*list)->next->next->prev = *list;
+					(*list)->next = (*list)->next->next;
+					(*list)->prev = tmp;
+					tmp->next = *list;
+					tmp->prev  = NULL;
+					*list = tmp;
+					head = *list;
+					print_list(head);
+					counter+= 1;
+					*list = (*list)->next;
+					continue;
+				}
+			}
+			if((*list)->n > (*list)->next->n)
+			{
+				s_tmp = (*list)->next;
+				s_tmp->prev = (*list)->prev;
+				(*list)->next = (*list)->next->next;
+				(*list)->prev->next = s_tmp;
+				(*list)->prev = s_tmp;
+				s_tmp->next = *list;
+				*list = s_tmp;
+				print_list(head);
+				counter+= 1;
+				if ((*list)->prev->n > (*list)->n)
+				{
+					while((*list)->n < (*list)->prev->n)
+					{
+						tmp = (*list)->prev;
+						if (tmp->prev != NULL)
+						{
+							tmp->prev->next = *list;
+							(*list)->prev = (*list)->prev->prev;
+							if ((*list)->next != NULL)
+							{
+								tmp->next = (*list)->next;
+								(*list)->next->prev = tmp;
+							}
+							else
+								tmp->next = NULL;
+							(*list)->next = tmp;
+							tmp->prev = *list;
+							print_list(head);
 
-                        }
-                        else
-                        {
-                            tmp->next = (*list)->next;
-                            (*list)->next->prev = tmp;
-                            tmp->prev = *list;
-                            (*list)->prev = NULL;
-                            (*list)->next = tmp;
-                            head = *list;
-                            print_list(head);
-                            break;
-                        }
-                    }
+						}
+						else
+						{
+							tmp->next = (*list)->next;
+							(*list)->next->prev = tmp;
+							tmp->prev = *list;
+							(*list)->prev = NULL;
+							(*list)->next = tmp;
+							head = *list;
+							print_list(head);
+							break;
+						}
+					}
 
-                }
-            }
-            *list = (*list)->next;
-        }
-    }
-    *list = head;
+				}
+			}
+			*list = (*list)->next;
+		}
+	}
+	*list = head;
 }
