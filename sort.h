2646ba8e34d0887269d2a489cfe481c5f4cc939c
PabloOsorix <3387@holbertonschool.com>
2022-02-15 19:28:39 -0500

    :heavy_plus_sign: update sort header

--- sort.h
+++ sort.h
@@ -24,5 +24,9 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void swap_nodes(listint_t **list, listint_t *left, listint_t *right);
+void quick_sort(int *array, size_t size);
+void real_quick_sort(int *array, int low, int high, size_t size);
+int partition(int *array, int low, int high, size_t size);
+void swap(int *a, int *b);
 
 #endif
